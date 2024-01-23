d2ce99257a1197440885bf7b2229b5b420424c09
Lee-Kyung-Sup(이경섭) <120997897+Lee-Kyung-Sup@users.noreply.github.com>
2024-01-23 19:40:17 +0900

    added functions
    1.character choose
    2.name input
    3.animation added

---
+++ Assets/Scripts/UI/PopupStartMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupStartMenu : MonoBehaviour
+{
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Text playerName;
+    [SerializeField] private GameObject information;
+    [SerializeField] private GameObject selectCharacter;
+
+    public void OnClickCharacter()
+    {
+        information.SetActive(false);
+        selectCharacter.SetActive(true);
+    }
+
+    public void OnClickJoin()
+    {
+        if(!(2<inputField.text.Length && inputField.text.Length<10))
+        {
+            return;
+        }
+
+        playerName.text= inputField.text;
+
+        Destroy(gameObject);
+    }
+}
